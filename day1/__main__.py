72fd03d5a1ef3bc4a543229e589b83a19d31a2d5
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-12-07 21:24:32 +0100

    Solve part 1

--- day1/__main__.py
+++ day1/__main__.py
@@ -1,11 +1,16 @@
 from pathlib import Path
+
 from . import lib
 
-
-with open(Path(__file__).parent.joinpath("data/input.txt"), 'r', encoding="utf-8") as f:
+with open(Path(__file__).parent.joinpath("data/input.txt"), "r", encoding="utf-8") as f:
     lines = f.read().split("\n")
 
-part1 = None
+
+sum = 0
+for line in lines:
+    sum += lib.first_and_last_number_as_int(line)
+
+part1 = sum
 part2 = None
 
 print(f"Part 1: {part1}")
