6356377f3513292785b1fd494dd2e7ecec4e6dd2
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-12-09 10:08:41 +0100

    Finish part 1

--- day3/lib.py
+++ day3/lib.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 def is_symbol(s: str) -> bool:
     return s not in ".1234567890"
 
@@ -13,3 +16,93 @@ def pad_input(lines: list[str]) -> list[str]:
     padded_lines.append(pad_row)
 
     return padded_lines
+
+
+@dataclass(frozen=True)
+class Pos:
+    row: int
+    col: int
+
+    def maybe_adjust_eol(self, width: int):
+        if self.col < width:
+            return self
+
+        return self.__class__(self.row + 1, 0)
+
+
+@dataclass
+class Parsed:
+    parsed: str
+    start_pos: Pos
+    next_pos: Pos
+
+    def __len__(self) -> int:
+        return len(self.parsed)
+
+    def __int__(self) -> int:
+        return int(self.parsed)
+
+    def __bool__(self) -> bool:
+        return bool(self.parsed)
+
+
+def get_number_at_position(lines: list[str], pos: Pos) -> Parsed:
+    line = lines[pos.row]
+    width = len(line)
+    if not line[pos.col].isdigit():
+        return Parsed(
+            "", pos, next_pos=Pos(pos.row, pos.col + 1).maybe_adjust_eol(width)
+        )
+
+    end = pos.col
+    while end < len(line) and line[end].isdigit():
+        end += 1
+
+    return Parsed(
+        line[pos.col : end], pos, next_pos=Pos(pos.row, end).maybe_adjust_eol(width)
+    )
+
+
+def neighbors_symbol(padded_lines: list[str], parsed: Parsed) -> bool:
+    def is_symbol_at_pos(row: int, col: int) -> bool:
+        return is_symbol(padded_lines[row + 1][col + 1])
+
+    row = parsed.start_pos.row
+    start_col = parsed.start_pos.col
+    outer_left_col = parsed.start_pos.col - 1
+    outer_right_col = parsed.start_pos.col + len(parsed)
+
+    # symbol above
+    for c in padded_lines[row][start_col + 1 : outer_right_col + 1]:
+        if is_symbol(c):
+            return True
+
+    # symbol below
+    for c in padded_lines[row + 2][start_col + 1 : outer_right_col + 1]:
+        if is_symbol(c):
+            return True
+
+    return any(
+        [
+            is_symbol_at_pos(row - 1, outer_left_col),
+            is_symbol_at_pos(row, outer_left_col),
+            is_symbol_at_pos(row + 1, outer_left_col),
+            is_symbol_at_pos(row - 1, outer_right_col),
+            is_symbol_at_pos(row, outer_right_col),
+            is_symbol_at_pos(row + 1, outer_right_col),
+        ]
+    )
+
+def part1(lines: list[str]) -> int:
+    padded_lines = pad_input(lines)
+
+    sum = 0
+    pos = Pos(0, 0)
+    while pos.row < len(lines):
+        parsed = get_number_at_position(lines, pos)
+        if parsed and neighbors_symbol(padded_lines, parsed):
+            sum += int(parsed)
+
+        pos = parsed.next_pos
+
+    return sum
--- day3/test.py
+++ day3/test.py
@@ -2,6 +2,7 @@ import unittest
 from pathlib import Path
 
 import lib
+from lib import Parsed, Pos
 
 with open(
     Path(__file__).parent.joinpath("data/test_input.txt"), "r", encoding="utf-8"
@@ -31,6 +32,52 @@ class TestArray(unittest.TestCase):
             "..."
         ], lib.pad_input(["#"]))
         # fmt: on
+
+
+class TestParse(unittest.TestCase):
+    def check_parsed(self, num: int, parsed: Parsed):
+        if num is None:
+            self.assertEqual("", parsed.parsed)
+        else:
+            self.assertEqual(num, int(parsed))
+
+    def test_parse_number(self):
+        self.check_parsed(467, lib.get_number_at_position(test_input_lines, Pos(0, 0)))
+        self.check_parsed(114, lib.get_number_at_position(test_input_lines, Pos(0, 5)))
+
+    def test_parse_null(self):
+        self.check_parsed(None, lib.get_number_at_position(test_input_lines, Pos(1, 0)))
+
+    def test_parse_number_eol(self):
+        self.assertEqual(
+            Parsed("", start_pos=Pos(0, 9), next_pos=Pos(1, 0)),
+            lib.get_number_at_position(test_input_lines, Pos(0, 9)),
+        )
+
+
+class TestFindSymbol(unittest.TestCase):
+    def setUp(self):
+        self.padded_lines = lib.pad_input(test_input_lines)
+
+    def test_neighbors_symbol(self):
+        self.assertTrue(
+            lib.neighbors_symbol(self.padded_lines, Parsed("467", Pos(0, 0), Pos(0, 3)))
+        )
+
+    def test_neighbors_symbol_above(self):
+        self.assertTrue(
+            lib.neighbors_symbol(self.padded_lines, Parsed("35", Pos(2, 2), Pos(2, 4)))
+        )
+
+    def test_does_not_neighbor_symbol(self):
+        self.assertFalse(
+            lib.neighbors_symbol(self.padded_lines, Parsed("114", Pos(0, 5), Pos(0, 8)))
+        )
+
+
+class TestPart1(unittest.TestCase):
+    def test_part1(self):
+        self.assertEqual(4361, lib.part1(test_input_lines))
 
 
 tc = unittest.TestCase()
