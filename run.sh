7421f17b8986e1d968f92b5f01085df4e868d744
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-12-16 08:40:27 +0100

    Add possibility to skip production run if it takes too much time

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 day="$1"
+shift
+
+run_prod=true
+for arg in "$@"; do
+    case $arg in
+        --skip-prod*)
+            run_prod=false
+        ;;
+        *)
+            # unknown option
+        ;;
+    esac
+done
 
 python3 -m unittest discover "$day" && echo "Tests passed"
-python3 -m "$day"
+
+if $run_prod; then
+    python3 -m "$day"
+fi
