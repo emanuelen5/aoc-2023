d5276bef182daf359b9a03ad3d9f84daf86652ee
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-12-20 08:26:44 +0100

    Create day8 from template

---
+++ day8/test.py
@@ -0,0 +1,9 @@
+import unittest
+from pathlib import Path
+
+import lib
+
+with open(
+    Path(__file__).parent.joinpath("data/test_input.txt"), "r", encoding="utf-8"
+) as f:
+    test_input_lines = f.read().split("\n")
